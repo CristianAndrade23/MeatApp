bef2cc2541d4ec046be3616d214298a471603932
cristian <cristian.a.s.andrade@hotmail.com>
2019-04-16 14:51:27 -0300

    serviço http para o detalhe de restaurante

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,61 +1,28 @@
 import {Restaurant} from './restaurant/restaurant.model';
+import {Injectable} from '@angular/core';
+import {MEAT_API} from '../app.api';
+import {Http} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import {ErrorHandler} from '../app.error-handler';
 
+@Injectable()
 export class RestaurantsService {
 
-  rests: Restaurant[] = [
-        {
-          id: 'bread-bakery',
-          name: 'Bread & Bakery',
-          category: 'Bakery',
-          deliveryEstimate: '25m',
-          rating: 4.9,
-          imagePath: 'assets/img/restaurants/breadbakery.png'
-        },
-        {
-          id: 'burger-house',
-          name: 'Burger House',
-          category: 'Hamburgers',
-          deliveryEstimate: '100m',
-          rating: 3.5,
-          imagePath: 'assets/img/restaurants/burgerhouse.png'
-        },
-        {
-          id: 'coffee-corner',
-          name: 'Coffee Corner',
-          category: 'Coffee Shop',
-          deliveryEstimate: '30-40m',
-          rating: 4.8,
-          imagePath: 'assets/img/restaurants/coffeecorner.png'
-        },
-        {
-          id: 'green-food',
-          name: 'Green Food',
-          category: 'Saudável',
-          deliveryEstimate: '75m',
-          rating: 4.1,
-          imagePath: 'assets/img/restaurants/greenfood.png'
-        },
-        {
-          id: 'ice-cream',
-          name: 'Ice Cream',
-          category: 'Ice Creams',
-          deliveryEstimate: '40-65m',
-          rating: 4.5,
-          imagePath: 'assets/img/restaurants/icy.png'
-        },
-        {
-          id: 'tasty-treats',
-          name: 'Tasty Treats',
-          category: 'Doces',
-          deliveryEstimate: '20m',
-          rating: 4.4,
-          imagePath: 'assets/img/restaurants/tasty.png'
-        }
-      ];
+  constructor(private http: Http){}
 
-  constructor(){}
-
-  restaurants() {
-    return this.rests;
+  restaurants(): Observable<Restaurant[]> {
+    return this.http.get(`${ MEAT_API }/restaurants`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
   };
+
+
+  restaurantsPorId(id: number): Observable<Restaurant>{
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError)
+  }
+
 }
